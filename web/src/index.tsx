9b479bdd5a0b9e7824036ee9f4c8e1633f0e6847
takashi.suzuki <42316228+k-tokitoh@users.noreply.github.com>
2021-02-15 15:03:36 +0900

    prevent double submission

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -7,6 +7,7 @@ type Message = { id?: string; body: string };
 
 const App: FC = () => {
   const [messages, setMessages] = useState<Message[]>([]);
+  const [isSubmitting, setSubmitting] = useState<boolean>(false);
   const [socket, setSocket] = useState<SocketIOClient.Socket>();
   useEffect(() => {
     setSocket(io(process.env.WEBSOCKET_ENDPOINT));
@@ -14,7 +15,11 @@ const App: FC = () => {
 
   const { register, handleSubmit, reset } = useForm<Message>();
   const onSubmit = handleSubmit(async (data) => {
-    socket?.emit("message", data, () => reset());
+    setSubmitting(true);
+    socket?.emit("message", data, () => {
+      reset();
+      setSubmitting(false);
+    });
   });
 
   socket?.on("message", (message: Message) => {
@@ -30,7 +35,7 @@ const App: FC = () => {
       </ul>
       <form onSubmit={onSubmit}>
         <input type="text" ref={register({ required: true })} name="body" />
-        <button>post</button>
+        <button disabled={isSubmitting}>post</button>
       </form>
     </>
   );
